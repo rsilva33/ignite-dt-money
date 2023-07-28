2726465b103a4ec186c7af681f85e1fd68929d46
Ronaldo Silva <ronaldo.silva.jr96@gmail.com>
2023-07-28 10:44:58 -0300

    Component: header

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,10 @@
+// Extendendo a tipagem e nao sobrescrever
+import 'styled-components';
+
+import { defaultTheme } from "../styles/themes/default";
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
