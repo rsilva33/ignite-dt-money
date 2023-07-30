10b462242e64b0d42fcf225fc83be66f7cad7b11
Ronaldo Silva <ronaldo.silva.jr96@gmail.com>
2023-07-30 18:39:49 -0300

    entry and exit buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from "phosphor-react";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
-import { CloseButton, Content, Overlay } from './styles';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
 export function NewTransactionModal() {
   return (
@@ -19,6 +19,17 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Description" required />
           <input type="number" placeholder="Price" required />
           <input type="text" placeholder="Category" required />
+          
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Income
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Outcome
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">
             Register
